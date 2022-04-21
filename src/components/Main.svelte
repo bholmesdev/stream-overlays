6365c1941e01323de760be34d14dfded7e6dd197
bholmesdev <bholmesdev@gmail.com>
2022-04-20 21:01:10 -0400

    fix: maincam aspect ratio

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -32,12 +32,15 @@
 
   .camera-box__main {
     position: relative;
-    aspect-ratio: 1920 / 1080;
     grid-area: maincam;
+    // tweak to "1089" to include colored padding
+    // as long as the ::after is 1920 / 1080, we're golden!
+    aspect-ratio: 1920 / 1089;
     background: linear-gradient(-45deg, var(--gradient-slinkity));
 
     &::after {
       content: "";
+      aspect-ratio: 1920 / 1080;
       position: absolute;
       inset: 0.3rem;
       background: var(--color-primary-7);
